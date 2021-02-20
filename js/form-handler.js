ea908ba70f96beb3364c9e3f874c32dd56d05ccb
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-02-20 23:52:30 +0300

    fix minprice

---
+++ js/form-handler.js
@@ -0,0 +1,31 @@
+const adForm = document.querySelector('.ad-form');
+const placeType = adForm.querySelector('#type');
+const nightPrice = adForm.querySelector('#price');
+const checkIn = adForm.querySelector('#timein');
+const checkOut = adForm.querySelector('#timeout');
+
+const onCheckInOut = (evt) => {
+  (evt.target === checkIn) ? checkOut.value = evt.target.value : checkIn.value = evt.target.value;
+};
+
+[checkIn, checkOut].forEach((element) => element.addEventListener('change', onCheckInOut));
+
+placeType.addEventListener('change', () => {
+  let minPrice = 0;
+  switch (placeType.value) {
+    case 'bungalow':
+      minPrice = 0;
+      break;
+    case 'flat':
+      minPrice = 1000;
+      break;
+    case 'house':
+      minPrice = 5000;
+      break;
+    case 'palace':
+      minPrice = 10000;
+      break;
+  }
+  nightPrice.min = nightPrice.placeholder = minPrice;
+});
+
