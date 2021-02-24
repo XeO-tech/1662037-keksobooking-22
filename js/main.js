51dc2d10e5dc68f84fd5014e01bbc77347112245
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-02-24 13:42:26 +0300

    fix mentor comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 import {createObjectsArr} from './data.js';
-import {map, renderMap, getAddressByMarkerOnly, showAdsOnMap} from './map.js';
-import {handleForm, changeFormStatus} from './form-handler.js';
+import {handleMap} from './map.js';
+import {handleForm} from './form-handler.js';
 
-const adsList = createObjectsArr(10);
-
-const onMapLoaded = () => changeFormStatus('enabled');
-
-
-changeFormStatus('disabled');
-
-renderMap();
-
-map.on('load', onMapLoaded());
+handleMap(createObjectsArr(10));
 
 handleForm();
-
-showAdsOnMap(adsList);
-
-getAddressByMarkerOnly();
--- js/map.js
+++ js/map.js
@@ -1,31 +1,20 @@
 /* global L:readonly */
 
 import {fillCard} from './elements-generator.js';
+import {changeFormStatus} from './form-handler.js';
 
-const map = L.map('map-canvas')
-  .setView({
-    lat: 35.6825,
-    lng: 139.7593,
-  }, 12);
+const handleMap = (adsArray) => {
 
-const pinIcon = L.icon({
-  iconUrl: '/img/main-pin.svg',
-  iconSize: [36, 36],
-  iconAnchor: [18, 36],
-});
+  changeFormStatus('disabled');
 
-const mainMarker = L.marker(
-  {
-    lat:35.6825,
-    lng:139.7512,
-  },
-  {
-    draggable: true,
-    icon: pinIcon,
-  },
-)
+  const onMapLoaded = () => changeFormStatus('enabled');
 
-const renderMap = () => {
+  const map = L.map('map-canvas')
+    .setView({
+      lat: 35.6825,
+      lng: 139.7593,
+    }, 12);
+
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
@@ -33,36 +22,57 @@ const renderMap = () => {
     },
   ).addTo(map);
 
-  mainMarker.addTo(map);
+  const getAddressByMarkerOnly = () => {
+    const addressField = document.querySelector('#address');
+    addressField.readOnly = true;
+    mainMarker.on('moveend', (evt) => {
+      const coordinates = evt.target.getLatLng();
+      addressField.value = `${coordinates.lat.toFixed(5)}, ${coordinates.lng.toFixed(5)}`
+    });
+  }
 
-}
+  const showAdsOnMap = (adsArray) => {
+    const adsIcon = L.icon({
+      iconUrl: './img/pin.svg',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+    });
+    adsArray.forEach((element) => {
+      L.marker({
+        lat: element.location.x,
+        lng: element.location.y,
+      },
+      {
+        icon: adsIcon,
+      })
+        .addTo(map)
+        .bindPopup(fillCard(element));
+    });
+  };
 
-const getAddressByMarkerOnly = () => {
-  const addressField = document.querySelector('#address');
-  addressField.readOnly = true;
-  mainMarker.on('moveend', (evt) => {
-    const coordinates = evt.target.getLatLng();
-    addressField.value = `${coordinates.lat.toFixed(5)}, ${coordinates.lng.toFixed(5)}`
+
+  const pinIcon = L.icon({
+    iconUrl: './img/main-pin.svg',
+    iconSize: [36, 36],
+    iconAnchor: [18, 36],
   });
-}
 
-const showAdsOnMap = (adsArray) => {
-  const adsIcon = L.icon({
-    iconUrl: '/img/pin.svg',
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-  });
-  adsArray.forEach((element) => {
-    L.marker({
-      lat: element.location.x,
-      lng: element.location.y,
+  const mainMarker = L.marker(
+    {
+      lat:35.6825,
+      lng:139.7512,
     },
     {
-      icon: adsIcon,
-    })
-      .addTo(map)
-      .bindPopup(fillCard(element));
-  });
-};
+      draggable: true,
+      icon: pinIcon,
+    },
+  ).addTo(map);
 
-export {map, renderMap, getAddressByMarkerOnly, showAdsOnMap};
+  getAddressByMarkerOnly();
+
+  showAdsOnMap(adsArray);
+
+  map.on('load', onMapLoaded());
+}
+
+export {handleMap};
